b910176d290185138c9a08c034525a19a244697a
kazu744 <oyamakazuma1@gmail.com>
2025-10-03 22:58:20 +0900

    signup完了

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect
@@ -25,9 +26,9 @@ def signup():
     if form.validate_on_submit():
         new_user = User.signup(
             email = form.email.data,
-            password = form.password.data
+            password = form.password.data,
             )
         if new_user:
             flash("登録完了しました")
-            return redirect(url_for('login'))
+            return redirect(url_for('signup'))
     return render_template('signup.html', form=form)
